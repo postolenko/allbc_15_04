eb2b0c6d6587163a9d421ffbbea8afd2daaab0a8
postolenko <denis.postolenko@gmail.com>
2021-08-23 01:41:45 +0300

    ALLBC

--- README.md
+++ README.md
@@ -19,3 +19,5 @@
 # http://postolenko.github.io/allbc_15_04/messages.html
 # http://postolenko.github.io/allbc_15_04/list_messages.html
 # http://postolenko.github.io/allbc_15_04/statistik.html
+# http://postolenko.github.io/allbc_15_04/edit_bc.html
+# http://postolenko.github.io/allbc_15_04/edit_bc_2.html
--- js/map.js
+++ js/map.js
@@ -134,3 +134,33 @@ if( document.getElementById("location_map") ) {
 
     }
 }
+
+if( document.getElementById("bcMap") ) {
+    function initMap() {
+
+        var image2 = "img/marker.svg";
+
+        map2 = new google.maps.Map(document.getElementById('bcMap'), {
+            center: {lat: 55.882593, lng: 37.5477503},
+            scrollwheel: false,
+            scaleControl: false,
+            zoom: 16,
+            disableDefaultUI: true,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+
+        marker2 = new google.maps.Marker({
+            map: map2,
+            draggable: false,
+            animation: google.maps.Animation.DROP,
+            position: {lat: 55.882593, lng: 37.5477503},
+            icon: image2,
+            title: ''
+        });
+
+        // marker.addListener('click', toggleBounce);
+        // marker.addListener('click', showObject);
+
+    }
+}
+
